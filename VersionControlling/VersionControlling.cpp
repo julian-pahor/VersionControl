28b1f7410d265821ae6f7d77fd70ebc21a4715c4
julian-pahor <julian.pahor1998@gmail.com>
2022-05-30 15:53:32 +0930

    Updating!

---
+++ VersionControlling/VersionControlling.cpp
@@ -0,0 +1,24 @@
+// VersionControlling.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    for (int i = 0; i < 42; i++)
+    {
+        if (i == 39)
+        {
+            std::cout << "What number are we again?" << std::endl;
+
+            std::cout << i;
+
+            std::cout << "....Oh yea..." << std::endl;
+
+        }
+        
+    }
+}
+
